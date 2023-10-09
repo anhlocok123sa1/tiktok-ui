9c0872477981b97eb3bc97bffa75fa913d12de04
anhlocok123sa1 <100480904+anhlocok123sa1@users.noreply.github.com>
2023-10-10 06:36:05 +0700

    Incompleted menu item

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,21 +2,54 @@ import classNames from 'classnames/bind';
 import { useEffect, useState } from 'react';
 import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+    faPlus,
+    faEllipsisVertical,
+} from '@fortawesome/free-solid-svg-icons';
 
+import Button from '@/components/Button';
 import images from '@/assets/images';
 import styles from './Header.module.scss';
 import { Wrapper as PopperWrapper } from '@/components/Popper';
 import AccoutItem from '@/components/AccountItem';
+import Menu from '@/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <img src={images.creation} alt="creation" />,
+        title: 'LIVE Creator Hub',
+        to: '/live',
+    },
+    {
+        icon: <img src={images.languages} alt="languages" />,
+        title: 'English',
+    },
+    {
+        icon: <img src={images.feedback} alt="feedback" />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <img src={images.keyboard} alt="keyboard" />,
+        title: 'Keyboard shortcuts',
+    },
+    {
+        icon: <img src={images.viewmode} alt="viewmode" />,
+        title: 'Dark mode',
+    },
+];
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
 
     useEffect(() => {
         setTimeout(() => {
-            setSearchResult([1]);
+            setSearchResult([]);
         });
     }, []);
 
@@ -42,7 +75,7 @@ function Header() {
                     )}
                 >
                     <div className={cx('search')}>
-                        <input placeholder="Tìm kiếm" spellCheck={false} />
+                        <input placeholder="Search" spellCheck={false} />
 
                         <div className={cx('action-search')}>
                             <button className={cx('clear')}>
@@ -55,7 +88,17 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>
+                        Upload
+                    </Button>
+                    <Button primary>Log in</Button>
+                    <Menu items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
+                </div>
             </div>
         </header>
     );
